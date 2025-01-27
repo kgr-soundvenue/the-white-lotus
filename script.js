7eefa6a33ae43db52136119f199dd930dbce613b
Pilsgaard <casper@pilsgaard.dk>
2025-01-27 10:30:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -25,34 +25,6 @@ function getPageIndex(urlString) {
  *    - skip .w--current re-assignment (we handle that)
  *****************************************************/
 function resetWebflow(data) {
-  let dom = $(new DOMParser().parseFromString(data.next.html, "text/html")).find("html");
-  // reset webflow interactions
-  $("html").attr("data-wf-page", dom.attr("data-wf-page"));
-  window.Webflow && window.Webflow.destroy();
-  window.Webflow && window.Webflow.ready();
-  window.Webflow && window.Webflow.require("ix2").init();
-  // reset w--current class
-  $(".w--current").removeClass("w--current");
-  $("a").each(function () {
-    if ($(this).attr("href") === window.location.pathname) {
-      $(this).addClass("w--current");
-    }
-  });
-  // reset scripts
-  dom.find("[data-barba-script]").each(function () {
-    let codeString = $(this).text();
-    if (codeString.includes("DOMContentLoaded")) {
-      let newCodeString = codeString.replace(/window\.addEventListener\("DOMContentLoaded",\s*\(\s*event\s*\)\s*=>\s*{\s*/, "");
-      codeString = newCodeString.replace(/\s*}\s*\);\s*$/, "");
-    }
-    let script = document.createElement("script");
-    script.type = "text/javascript";
-    if ($(this).attr("src")) script.src = $(this).attr("src");
-    script.text = codeString;
-    document.body.appendChild(script).remove();
-  });
-  
-  /*
   console.log("[resetWebflow] START ----------------------------------");
 
   // Convert the next page's HTML to a DOM, grab its <html> node
@@ -83,14 +55,14 @@ function resetWebflow(data) {
     const srcAttr = $this.attr("src");
 
     console.log("[resetWebflow] Re-injecting script:", srcAttr || "[inline code]");
-*/
+
     // Strip out any extra DOMContentLoaded wrapper if present
-//    if (codeString.includes("DOMContentLoaded")) {
-//      codeString = codeString
-//        .replace(/window\.addEventListener\("DOMContentLoaded".*?\{\s*/, "")
-//        .replace(/\s*\}\)\s*;\s*$/, "");
-//    }
-/*
+    if (codeString.includes("DOMContentLoaded")) {
+      codeString = codeString
+        .replace(/window\.addEventListener\("DOMContentLoaded".*?\{\s*/, "")
+        .replace(/\s*\}\)\s*;\s*$/, "");
+    }
+
     // Create a script in the document
     let script = document.createElement("script");
     script.type = "text/javascript";
@@ -123,7 +95,7 @@ function resetWebflow(data) {
   }
 
   console.log("[resetWebflow] END ------------------------------------");
-  */
+  
 }
 
 /**************************************************************
